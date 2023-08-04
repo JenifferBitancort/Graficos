4c47a14ccc10416a8289f0c8a232c74027992780
Jeniffer Bitancort <jenifferbitancort@gmail.com>
2023-08-04 15:07:52 -0300

    Commit inicial

---
+++ Graficos/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace _37___Graficos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.buttonInserir = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.comboTipoGrafico = new System.Windows.Forms.ComboBox();
+            this.comboCores = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonValoresAle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grafico = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gráfico";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor de X";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(168, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor de Y";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxX.Location = new System.Drawing.Point(21, 82);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(119, 26);
+            this.textBoxX.TabIndex = 3;
+            this.textBoxX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxX_KeyPress);
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxY.Location = new System.Drawing.Point(151, 82);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(119, 26);
+            this.textBoxY.TabIndex = 4;
+            this.textBoxY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxY_KeyPress);
+            // 
+            // buttonInserir
+            // 
+            this.buttonInserir.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInserir.Location = new System.Drawing.Point(21, 124);
+            this.buttonInserir.Name = "buttonInserir";
+            this.buttonInserir.Size = new System.Drawing.Size(250, 36);
+            this.buttonInserir.TabIndex = 5;
+            this.buttonInserir.Text = "Inserir valores no Gráfico";
+            this.buttonInserir.UseVisualStyleBackColor = true;
+            this.buttonInserir.Click += new System.EventHandler(this.buttonInserir_Click);
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLimpar.Location = new System.Drawing.Point(21, 343);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(250, 36);
+            this.buttonLimpar.TabIndex = 6;
+            this.buttonLimpar.Text = "Limpar Gráfico";
+            this.buttonLimpar.UseVisualStyleBackColor = true;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // comboTipoGrafico
+            // 
+            this.comboTipoGrafico.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboTipoGrafico.FormattingEnabled = true;
+            this.comboTipoGrafico.Location = new System.Drawing.Point(21, 395);
+            this.comboTipoGrafico.Name = "comboTipoGrafico";
+            this.comboTipoGrafico.Size = new System.Drawing.Size(250, 25);
+            this.comboTipoGrafico.TabIndex = 7;
+            this.comboTipoGrafico.SelectedIndexChanged += new System.EventHandler(this.comboTipoGrafico_SelectedIndexChanged);
+            // 
+            // comboCores
+            // 
+            this.comboCores.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboCores.FormattingEnabled = true;
+            this.comboCores.Location = new System.Drawing.Point(291, 395);
+            this.comboCores.Name = "comboCores";
+            this.comboCores.Size = new System.Drawing.Size(250, 25);
+            this.comboCores.TabIndex = 8;
+            this.comboCores.SelectedIndexChanged += new System.EventHandler(this.comboCores_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(556, 399);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Gráfico 3D";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // buttonValoresAle
+            // 
+            this.buttonValoresAle.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonValoresAle.Location = new System.Drawing.Point(652, 385);
+            this.buttonValoresAle.Name = "buttonValoresAle";
+            this.buttonValoresAle.Size = new System.Drawing.Size(136, 43);
+            this.buttonValoresAle.TabIndex = 10;
+            this.buttonValoresAle.Text = "Inserir valores aleatórios";
+            this.buttonValoresAle.UseVisualStyleBackColor = true;
+            this.buttonValoresAle.Click += new System.EventHandler(this.buttonValoresAle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.x,
+            this.y});
+            this.dataGridView1.Location = new System.Drawing.Point(21, 166);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(250, 154);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // x
+            // 
+            this.x.HeaderText = "Eixo X";
+            this.x.Name = "x";
+            this.x.ReadOnly = true;
+            // 
+            // y
+            // 
+            this.y.HeaderText = "Eixo Y";
+            this.y.Name = "y";
+            this.y.ReadOnly = true;
+            // 
+            // grafico
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.grafico.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.grafico.Legends.Add(legend3);
+            this.grafico.Location = new System.Drawing.Point(291, 12);
+            this.grafico.Name = "grafico";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "X vs Y";
+            this.grafico.Series.Add(series3);
+            this.grafico.Size = new System.Drawing.Size(497, 356);
+            this.grafico.TabIndex = 12;
+            this.grafico.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 438);
+            this.Controls.Add(this.grafico);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonValoresAle);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboCores);
+            this.Controls.Add(this.comboTipoGrafico);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.buttonInserir);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Button buttonInserir;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.ComboBox comboTipoGrafico;
+        private System.Windows.Forms.ComboBox comboCores;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonValoresAle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafico;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Graficos/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _37___Graficos
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<double, double> valores;
+        int contadorX = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            valores = new Dictionary<double, double>();    
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxX.Focus();   
+
+            for (int i = 0; i < 35; i++)
+            {
+                comboTipoGrafico.Items.Add((SeriesChartType)i);   
+
+
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                comboCores.Items.Add((ChartColorPalette)i);   
+            }
+
+
+        }
+
+        private void buttonInserir_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(textBoxX.Text) || string.IsNullOrEmpty(textBoxY.Text))
+            {
+                MessageBox.Show("Os dois valores são obrigatórios");
+                return;
+            }
+
+            if (valores.ContainsKey(double.Parse(textBoxX.Text)))   
+            {
+                valores[double.Parse(textBoxX.Text)] = double.Parse(textBoxY.Text); 
+            }
+            else  
+            {
+                valores.Add(double.Parse(textBoxX.Text),double.Parse(textBoxY.Text)); 
+                string x = textBoxX.Text; 
+                string y = textBoxY.Text; 
+            }
+
+            var items = from valor in valores orderby valor.Key ascending select valor;   
+            dataGridView1.Rows.Clear();  
+            grafico.Series[0].Points.Clear();   
+
+            foreach (var item in items)   
+            {
+                dataGridView1.Rows.Add(item.Key, item.Value);  
+                grafico.Series[0].Points.AddXY(item.Key, item.Value);   
+            }
+
+            grafico.Update();   
+
+            textBoxX.Text = "";    
+            textBoxY.Text = "";     
+            textBoxX.Focus();       
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            textBoxX.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString();
+                        
+
+            textBoxY.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[1].Value.ToString();
+                       
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            valores.Clear();       
+            grafico.Series[0].Points.Clear();       
+            dataGridView1.Rows.Clear();             
+            contadorX = 0;         
+            textBoxX.Text = "";    
+            textBoxY.Text = "";    
+            textBoxX.Focus();       
+        }
+
+        private void comboTipoGrafico_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            grafico.Series[0].ChartType = (SeriesChartType)comboTipoGrafico.SelectedItem;   
+        }
+
+        private void comboCores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            grafico.Palette = (ChartColorPalette)comboCores.SelectedItem;
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            grafico.ChartAreas[0].Area3DStyle.Enable3D = checkBox1.Checked; 
+        }
+
+        private void buttonValoresAle_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;   
+        }
+
+        private void textBoxX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8 && e.KeyChar != (char)13 && e.KeyChar != (Char)44 ||
+                (textBoxX.Text.Contains((char)44) && e.KeyChar == (char)44))
+            {
+                e.Handled = true;  
+            }
+        }
+
+        private void textBoxY_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8 && e.KeyChar != (char)13 && e.KeyChar != (Char)44 ||
+                (textBoxY.Text.Contains((char)44) && e.KeyChar == (char)44))
+            {
+                e.Handled = true;   
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (grafico.Series[0].Points.Count > 10)   
+            {
+                grafico.Series[0].Points.RemoveAt(0);   
+                grafico.Update();        
+            }
+
+            double y = (double)new Random((int)DateTime.Now.Ticks).Next(1000);  
+            grafico.Series[0].Points.AddXY(contadorX++, y); 
+
+            dataGridView1.Rows.Add(contadorX, y);  
+            dataGridView1.CurrentCell = dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0];
+                          
+        }
+    }
+}
